bfb66d2ab70c7c96916cdc4d2601a5a70e678991
Atria Labs <atrialabs@outlook.com>
2022-09-08 07:26:08 +0000

    disable next image handling

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 const withMarkdoc = require('@markdoc/next.js');
 
 module.exports = withMarkdoc(/* options */)({
+  experimental: {
+      images: {
+          unoptimized: true,
+      }
+  },
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
